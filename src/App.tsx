4af54c61e9d5071e3c6eca6581d5e2ea33e6743f
Behruz <behruz@bictory.io>
2024-09-21 11:31:22 +0500

    init 1.6

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,17 @@ import './app.css'
 
 const App = () => {
   const [initDataUnsafe] = useInitData()
+
+  console.log(initDataUnsafe.user);
+  console.log(initDataUnsafe);
+
   return (
-    <div>{initDataUnsafe.user?.username || 'Ghost'}</div>
+    <div>
+      {initDataUnsafe.user?.username || 'Ghost'}
+
+      <h1>{JSON.stringify(initDataUnsafe)}</h1>
+      <h2>{JSON.stringify(initDataUnsafe.user)}</h2>
+    </div>
   )
 }
 
